3e7876832136eb2d75f19f9f44380f5646831640
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-12 18:55:47 +0700

    Add Classes example

---
+++ CSharpIntermediate/Classes/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduction(string to)
+        {
+            Console.WriteLine("Hi {0} I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person
+            {
+                Name = str
+            };
+
+            return person;
+        }
+    }
+}
---
+++ CSharpIntermediate/Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("Anawat");
+            person.Introduction("James");
+        }
+    }
+}
