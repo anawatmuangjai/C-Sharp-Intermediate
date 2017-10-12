82c3517b31fe5eca7cca0f37b4038ea0dfdcae89
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-12 19:39:43 +0700

    Add Object Initializers example

---
+++ CSharpIntermediate/ObjectInitializers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace ObjectInitializers
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object initializer
+            var person = new Person
+            {
+                FirstName = "Anawat",
+                LastName = "Muangjai"
+            };
+
+            Console.WriteLine("{0} {1}",person.FirstName, person.LastName);
+        }
+    }
+}
