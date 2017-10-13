9ac4185f4756977a6e8adb3666a4ca107f0f4a97
anawatmuangjai <anawat.muangjai@gmail.com>
2017-10-13 11:26:33 +0700

    Add Properties example

---
+++ CSharpIntermediate/Properties/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        // Auto implement properties (Auto implement private fields)
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var year = timeSpan.Days / 365;
+
+                return year;
+            }
+        }
+    }
+}
---
+++ CSharpIntermediate/Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Properties
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1993, 1, 1));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
