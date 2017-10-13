be32d49243eeb90a52ec0d6b619989794706ffe4
anawatmuangjai <anawat.muangjai@gmail.com>
2017-10-13 11:05:59 +0700

    Add Access Modifiers Encapsulation example

---
+++ CSharpIntermediate/AccessModifiers/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        // Encapsulation 
+        // Define fields as private
+        // Provide getter/setter methods as public
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
---
+++ CSharpIntermediate/AccessModifiers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1993, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
