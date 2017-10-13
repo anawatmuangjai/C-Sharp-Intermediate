8b36f7c7fc71750ed5de54ee7db334dceff012bf
anawatmuangjai <anawat.muangjai@gmail.com>
2017-10-13 11:55:16 +0700

    Add Inheritance example

---
+++ CSharpIntermediate/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+        }
+    }
+}
