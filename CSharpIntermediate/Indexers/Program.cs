5f271806654b20d9d143560960354b97d9ac99ed
anawatmuangjai <anawat.muangjai@gmail.com>
2017-10-13 11:38:39 +0700

    Add Indexers example

---
+++ CSharpIntermediate/Indexers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Anawat";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
